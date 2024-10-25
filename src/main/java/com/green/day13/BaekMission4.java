54a51ac434f83e2d3182ece7ab668b566be511f7
boyunsim <tlaqhdbs12@gmail.com>
2024-10-25 09:52:00 +0900

    24_10_25(별찍기 완료)

--- src/main/java/com/green/day13/BaekMission4.java
+++ src/main/java/com/green/day13/BaekMission4.java
@@ -9,17 +9,18 @@ public class BaekMission4 {
         for (int i = 0; i<2*n-1; i++){
             for(int z= 0; z<2*n-1; z++){
                 if(i<n) {
-                    if(n-i<=z && z<=n+i) {
+                    if(n-1-i<=z && z<=n-1+i) {
                         System.out.print("*");
-                    } else {
+                    } else if (n-1-i>z) {
                         System.out.print(" ");
                     }
                 }
-                if(i==n) {
-                    System.out.print("*");
-                }
-                if(i>n) {
-                    if(n-i<=z && z<=n+i)
+                if(i>=n) {
+                    if(n-1-((2*n-2)-i)<=z && z<=n-1+((2*n-2)-i)) {
+                        System.out.print("*");
+                    } else if(n-1-((2*n-2)-i) > z){
+                        System.out.print(" ");
+                    }
                 }
             }
             System.out.println();
