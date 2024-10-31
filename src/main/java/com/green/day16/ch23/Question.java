2c00d6ea1e8b7fde8759c5beb68ccf03420e0094
boyunsim <tlaqhdbs12@gmail.com>
2024-10-31 14:20:20 +0900

    24_10_31(final)

--- src/main/java/com/green/day16/ch23/Question.java
+++ src/main/java/com/green/day16/ch23/Question.java
@@ -4,35 +4,23 @@ import java.util.Arrays;
 
 public class Question {
     public static void main(String[] args) {
-        String str2 = "1a2b3c4d123";
+        Solution s = new Solution();
+        String str = new String("abCdEfghIJ");
+        System.out.println(s.solution(str));
 
-        int sum = 0;
-
-        for (int z = 1; z <= 9; z++) {
-            String str1 = str2;
-            for (int i = 0; i<str2.length(); i++){
-                if (str1.indexOf(z + "") != -1) {
-                    sum += z;
-                    str1 = str1.substring(str1.indexOf(z + "") + 1);
-                    System.out.println(z + ":" + str1);
-                }
+    }
+}
+class Solution {
+    public String solution(String my_string) {
+        char[] ch = my_string.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            if (Character.isUpperCase(ch[i])) {
+                ch[i] = Character.toLowerCase(ch[i]);
+            } else {
+                ch[i] = Character.toUpperCase(ch[i]);
             }
         }
-        System.out.println(sum);
+        return String.valueOf(ch);
     }
 }
-
-class Solution {
-    public String solution(String rsp) {
-        String rsp0 = rsp.replace("0", "6");
-        String rsp1 = rsp.replace("2", "7");
-        String rsp2 = rsp.replace("5", "8");
-        String rsp3 = rsp.replace("6", "5");
-        String rsp4 = rsp.replace("7", "0");
-        String answer = rsp.replace("8", "2");
-
-        return answer;
-    }
-}
-
 
